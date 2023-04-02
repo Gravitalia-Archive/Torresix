205b4bd829faa53098c0c4d9e36c075d6d775549
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-04-02 15:32:13 +0200

    Use ONNX instead of TF

--- src/model/mobilenet.rs
+++ src/model/mobilenet.rs
@@ -1,4 +1,4 @@
-use tract_tensorflow::prelude::*;
+use tract_onnx::prelude::*;
 use anyhow::Result;
 
 const RESULT: [&str; 1001] = [
@@ -1006,11 +1006,11 @@ const RESULT: [&str; 1001] = [
 ];
 
 pub fn init() -> Result<super::TensorflowModel> {
-    let model = tract_tensorflow::tensorflow()
-        .model_for_path("models/mobilenet_v2_1.4_224_frozen.pb")?
-        .with_input_fact(0, f32::fact([1, 224, 224, 3]).into())?
+    let model = tract_onnx::onnx()
+        .model_for_path("models/mobilenetv2-7.onnx")?
         .into_optimized()?
         .into_runnable()?;
+
     
     Ok(model)
 }
@@ -1020,16 +1020,20 @@ pub fn predict(model: super::TensorflowModel, buffer: Vec<u8>) -> Result<String>
     
     let resized =
         image::imageops::resize(&image, 224, 224, ::image::imageops::FilterType::Triangle);
-    let image: Tensor = tract_ndarray::Array4::from_shape_fn((1, 224, 224, 3), |(_, y, x, c)| {
-        resized[(x as _, y as _)][c] as f32 / 255.0
+    let image: Tensor = tract_ndarray::Array4::from_shape_fn((1, 3, 224, 224), |(_, c, y, x)| {
+        let mean = [0.485, 0.456, 0.406][c];
+        let std = [0.229, 0.224, 0.225][c];
+        (resized[(x as _, y as _)][c] as f32 / 255.0 - mean) / std
     })
     .into();
 
-    let best = model.run(tvec!(image.into()))?[0]
+    let result = model.run(tvec!(image.into()))?;
+
+    let best = result[0]
         .to_array_view::<f32>()?
         .iter()
         .cloned()
-        .zip(1..)
+        .zip(2..)
         .max_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
 
     Ok(RESULT[best.unwrap().1-1].to_string())
