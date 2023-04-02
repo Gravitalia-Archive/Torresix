c5916d81ee120d4553741e9f1f5fdf785234472a
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-04-02 15:33:00 +0200

    Implement ONXX

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,2 +1,4 @@
-pub type TensorflowModel = tract_tensorflow::prelude::SimplePlan<tract_tensorflow::prelude::TypedFact, Box<dyn tract_tensorflow::prelude::TypedOp>, tract_tensorflow::prelude::Graph<tract_tensorflow::prelude::TypedFact, Box<dyn tract_tensorflow::prelude::TypedOp>>>;
+pub type TensorflowModel = tract_onnx::prelude::SimplePlan<tract_onnx::prelude::TypedFact, Box<dyn tract_onnx::prelude::TypedOp>, tract_onnx::prelude::Graph<tract_onnx::prelude::TypedFact, Box<dyn tract_onnx::prelude::TypedOp>>>;
+
 pub mod mobilenet;
+pub mod grenade;
