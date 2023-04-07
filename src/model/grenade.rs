9219566c73c36bbcb574baa8b52f0dc8a68fae5c
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-04-07 16:23:29 +0200

    Fix grenade model

--- src/model/grenade.rs
+++ src/model/grenade.rs
@@ -15,7 +15,8 @@ const RESULT: [&str; 8] = [
 pub fn init() -> Result<super::TensorflowModel> {
     let model = tract_onnx::onnx()
         .model_for_path("./models/grenade.onnx")?
-        .with_input_fact(0, f32::fact([64, 224, 224, 3]).into())?
+        .with_input_fact(0, f32::fact([1, 224, 224, 3]).into())?
+        .with_output_fact(0, InferenceFact::dt_shape(f32::datum_type(), tvec![1, 8]))?
         .into_optimized()?
         .into_runnable()?;
     
@@ -23,23 +24,22 @@ pub fn init() -> Result<super::TensorflowModel> {
 }
 
 pub fn predict(model: super::TensorflowModel, buffer: Vec<u8>) -> Result<String> {
-    let image = image::load_from_memory(&buffer)?;
-    let resized =
-        image::imageops::resize(&image, 224, 224, ::image::imageops::FilterType::Triangle);
+    let img = image::load_from_memory(&buffer)?;
+    let resized_img = image::imageops::resize(&img, 224, 224, ::image::imageops::FilterType::Nearest); //img.resize_exact(224, 224, image::imageops::FilterType::Nearest);
 
-    let image: Tensor = tract_ndarray::ArrayBase::from_shape_fn((64, 3, 224, 224), |(_, c, y, x)| {
-        let mean = [0.485, 0.456, 0.406][c];
-        let std = [0.229, 0.224, 0.225][c];
-        (resized[(x as _, y as _)][c] as f32 / 255.0 - mean) / std
-    })
-    .into();
+    let img_array: Tensor = tract_ndarray::Array::from_shape_fn((1, 3, 224, 224), |(_, c, y, x)| {
+        resized_img.get_pixel(x as u32, y as u32)[c] as f32
+    }).into();
 
-    let best = model.run(tvec!(image.permute_axes(&[0, 2, 3, 1])?.into()))?[0]
-        .to_array_view::<f32>()?
-        .iter()
-        .cloned()
-        .zip(1..)
-        .max_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+
+    let outputs = model.run(tvec!(img_array.permute_axes(&[0, 2, 3, 1])?.into()))?;
+
+    let best = outputs[0]
+    .to_array_view::<f32>()?
+    .iter()
+    .cloned()
+    .zip(1..)
+    .max_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
 
     Ok(RESULT[best.unwrap().1-1].to_string())
 }
